29752b9fa2a9e396a9e9b4595774cc1b088dca3d
Fabio Choi <fchoi@Fabios-MBP.hitronhub.home>
2022-07-29 11:06:20 -0400

    fixed workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
 const { resolve } = require('path');
 
 const PUBLIC_PATH = resolve(__dirname, 'public');
+const BUILD_PATH = resolve(__dirname, 'dist');
+const LIB_PATH = resolve(__dirname, 'lib');
+
 const SRC_PATH = resolve(__dirname, 'src');
 const webpack = require('webpack');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
 	plugins: [
+		new CopyPlugin({
+			patterns: [
+			  {
+				from: `${PUBLIC_PATH}/index.html`,
+				to: `${BUILD_PATH}/index.html`,
+			  },{
+				from: `${PUBLIC_PATH}/styles.css`,
+				to: `${BUILD_PATH}/styles.css`,
+			  },
+			  {
+				from: `${LIB_PATH}/templates.js`,
+				to: `${BUILD_PATH}/templates.js`,
+			  },
+			],
+		  }),
 		new webpack.ProvidePlugin({
 			$: 'jquery',
 			jQuery: 'jquery',
@@ -17,7 +36,7 @@ module.exports = {
 	entry: `${SRC_PATH}/index.js`,
 	output: {
 		filename: 'bundle.js',
-		path: PUBLIC_PATH,
+		path: BUILD_PATH,
 	},
 
 	module: {
